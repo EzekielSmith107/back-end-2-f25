c7476672e8820adbf0dde59bea85470b49a856e5
Ezekiel Smith <ezesmith107@gmail.com>
2022-07-06 17:24:51 -0400

    Completed lab

--- server/controller.js
+++ server/controller.js
@@ -1,11 +1,52 @@
 const houses = require("./db.json")
+let globalId = houses[houses.length - 1].id + 1;
 
-// module.exports = {
-//     getHouses:
+module.exports = {
+    getHouses:(req, res) => {
+        res.status(200).send(houses);
+    },
 
-//     deleteHouse:
+    deleteHouse: (req, res) => {
+        const existingId = +req.params.id;
 
-//     createHouse:
+        let index = houses.findIndex(house => house.id === existingId);
+        houses.splice(index, 1);
+        res.status(200).send(houses);
+    },
 
-//     updateHouse:
-// }
+    createHouse: (req, res) => {
+        const {address, price, imageURL} = req.body;
+
+        let newHouse = {
+            address,
+            price,
+            imageURL,
+            id: globalId
+        }
+        
+        houses.push(newHouse);
+        res.status(200).send(houses);
+        globalId++
+    },
+
+    updateHouse: (req, res) => {
+        const existingId = +req.params.id;
+        let adjustType = req.body.type;
+        let index = houses.findIndex(house => house.id === existingId);
+
+        if(adjustType === "plus") {
+            houses[index].price += 10000;
+            res.status(200).send(houses);
+        } else if(adjustType === "minus") {
+          if(houses[index].price >= 10000) {
+            houses[index].price -= 10000;
+            res.status(200).send(houses);
+          } else if(houses[index].price < 10000 && houses[index].price > 0) {
+                houses[index].price = 0;
+                res.status(200).send(houses);
+          } else {
+                res.status(400).send("Cannot list a house below 0.");
+          }
+        }
+    }
+};
--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const {getHouses, createHouse, updateHouse, deleteHouse} = require("./controller.js");
 
+app.get("/api/houses", getHouses);
+app.post("/api/houses", createHouse);
+app.put("/api/houses/:id", updateHouse);
+app.delete("/api/houses/:id", deleteHouse);
 
 app.listen(4004, () => console.log("Docked at port 4004."));
